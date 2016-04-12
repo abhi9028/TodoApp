25770b86427132c9d6af68ccb098ad01d01bbfc7
Abhinav Srivastav <abhinav.srivastava@ongraph.com>
2016-04-12 12:22:24 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todoApp', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
+});
